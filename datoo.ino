ffe647f4fd31c5c4f2408974591acdb2136ef005
JXAS <64651576+JXAS@users.noreply.github.com>
2023-07-21 20:51:48 +0200

    Update datoo.ino
    Not Running. Edition in progress.

--- datoo.ino
+++ datoo.ino
@@ -155,6 +155,85 @@ int init_TM (void)
 return 0;
 }
 
+void DisplayDatas (void)
+{
+   unsigned int mask[16]={1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,\
+16384,32768};
+char buf[5],config[5];
+   int result,*intptr,intvalue;
+   long retard;
+
+   //XBee vars
+   char xbeestr[2048];
+   int longueur, x1,x2,x3,test;
+
+   char buffer[6];
+
+   buf[4]=0x00;
+   config[4]=0x00;
+   strncpy(config, &conf[12],4);
+   result=numhexstr2bin (config, buf, 2 );
+   intptr= (int*)buf;
+   nb_item=*intptr;
+   strncpy(config, &conf[8],4);
+   result=numhexstr2bin (config, buf, 4 );
+   intptr= (int*)buf;
+   looptime=*intptr;
+
+   for (n=0;n<nb_item;n++)
+   {
+	   strncpy(config, conf+24+n*12,4);
+	   result=numhexstr2bin (config, buf, 2 );
+	   intptr= (int*)buf;
+      intvalue=*intptr;
+   	my_xbee_module[n].conf= intvalue;
+//	   printf("config=%s intvalue=%u\n",config,intvalue);
+   }
+   printf("\nID : %s\n",my_stream.id);
+   printf("Version : %i.%i\n",(my_stream.version&0xFF00)>>8, my_stream.version&0x00FF);
+   printf("XBee-Detected : %04X\n",my_stream.xbee_detected);
+   printf("In's : %02X\n",(my_stream.ios & 0xff00)>>8);
+   printf("Out's : %02X\n\n",my_stream.ios & 0x00ff);
+
+   printf("NbItems=%u\n",nb_item);
+	for (n=0;n<nb_item;n++)
+   {
+	   printf("Module #%u\n\tID : %s Conf=%u\n",n,\
+      my_xbee_module[n].module_id,my_xbee_module[n].conf);
+		if ((my_xbee_module[n].conf)&mask[0])
+	      printf("\tLocalTemp : %i\n",my_xbee_module[n].local_temp);
+		if ((my_xbee_module[n].conf)&mask[1])
+	      printf("\tAnalogInput : %i\n",my_xbee_module[n].analog_input);
+		if ((my_xbee_module[n].conf)&mask[2])
+	      printf("\tXBeeBattery : %i\n",my_xbee_module[n].battery);
+		if ((my_xbee_module[n].conf)&mask[3])
+	      printf("\tPower-230VAC #1\n");
+		if ((my_xbee_module[n].conf)&mask[4])
+	      printf("\tPower-230VAC #2\n");
+		if ((my_xbee_module[n].conf)&mask[5])
+	      printf("\tSprinkle #1\n");
+		if ((my_xbee_module[n].conf)&mask[6])
+	      printf("\tSprinkle #2\n\n");
+      if ((my_xbee_module[n].conf)&mask[7])
+      {
+         printf("\tXBeeAlarmCount : %i\n",my_xbee_module[n].xbee_alarm_count);
+			xbee_alarm[nb_xbee_alarm]=n;
+         nb_xbee_alarm++;
+      }
+   }
+   printf("Nombre de XBee Alarme=%u\n",nb_xbee_alarm);
+
+   for (n=0;n<nb_xbee_alarm;n++)
+   {
+   	alarm_delay[xbee_alarm[n]]=SEC_TIMER;
+   	my_xbee_module[n].module_id[8]=0;
+   	printf("%u : %s\n",n,my_xbee_module[xbee_alarm[n]].module_id);
+   }
+
+
+}
+
+
 void GetCommandFromWeb (void)
 {
 }
